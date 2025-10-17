d592b43b86b3d446babc2b9649474cc8ee3ee316
MuhammedZeki <muhammed.codes@gmail.com>
2025-10-17 18:43:00 +0300

    HomePage responsive done ,mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,29 @@ import { useState } from "react";
 import SearchBar from "../_components/home/SearchBar";
 import "../css/home/HomePage.css";
 import WeatherImg from "../assets/images/icon-sunny.webp";
+import BottomArrow from "../assets/images/icon-dropdown.svg";
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 const HomePage = () => {
   const [inputText, setInputText] = useState("");
+  const [day, setDay] = useState("Monday");
+  const [isShow, setIsShow] = useState(false);
+  const handleDays = (e) => {
+    const { value } = e.target;
+    setDay(value);
+    setIsShow(!isShow);
+  };
+  const handleClickBtn = () => {
+    setIsShow(!isShow);
+  };
+  console.log(isShow);
   return (
     <div className="main-home">
       <SearchBar inputText={inputText} setInputText={setInputText} />
@@ -107,7 +128,124 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-        <div className="right-content"></div>
+        <div className="right-content">
+          <div className="hourly-container">
+            <div className="hourly-forecast-header">
+              <span>Hourly forecast</span>
+              <div
+                className={`hourly-forecast-dropdown `}
+                onClick={handleClickBtn}
+              >
+                <p>{day}</p>
+                <img src={BottomArrow} alt="BottomArrow" />
+              </div>
+              <div className={`days ${isShow === true ? "opened" : "closed"}`}>
+                {daysOfWeek.map((dayName) => (
+                  <label
+                    key={dayName}
+                    className={`day ${day === dayName ? "active" : ""}`}
+                    htmlFor={dayName}
+                  >
+                    {dayName}
+                    <input
+                      type="radio"
+                      id={dayName}
+                      value={dayName}
+                      onChange={handleDays}
+                      checked={day === dayName}
+                    />
+                  </label>
+                ))}
+              </div>
+            </div>
+            <div className="hourly-weather-cards">
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+              <div className="weathers-card">
+                <div className="weather-icon">
+                  <img src={WeatherImg} alt="" />
+                  <p>3 PM</p>
+                </div>
+                <p>20°</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
